9fa095ef569d8ba33dbf8942eb19aecd16aa333f
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-10-11 17:14:25 -0700

    Version bump.

--- NASB Voice Mod/PluginInfo.cs
+++ NASB Voice Mod/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "com.steven.nasb.voicemod";
         public const string PLUGIN_NAME = "Voice Mod";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.1.1";
     }
 }
